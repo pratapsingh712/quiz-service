bb024123720a7c2d09c998cade42f2e8cd853332
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-02-01 20:56:54 +0530

    implemented quiz interface properly with methods for url mapping

--- src/main/java/com/raghav/quiz_service/feign/QuizInterface.java
+++ src/main/java/com/raghav/quiz_service/feign/QuizInterface.java
@@ -4,15 +4,23 @@ import com.raghav.quiz_service.model.QuestionWrapper;
 import com.raghav.quiz_service.model.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 @FeignClient("QUESTION-SERVICE")
 public interface QuizInterface {
 
-    public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numQuestions);
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>>getQuestionsForQuiz(@RequestParam String categoryName, @RequestParam Integer numQuestions);
 
-    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds);
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionsIds);
 
-    public ResponseEntity<Integer> getScore(List<Response> responses);
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
 
 }
